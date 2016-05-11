632292aafeb62633fa41a4a64fa88e8042688949
jeffbuenting <jeffbuenting@gmail.com>
2016-05-11 16:33:22 -0400

    initial upload

---
+++ Get-HowCalled.ps1
@@ -0,0 +1,21 @@
+﻿# ----- http://tommymaynard.com/ql-determine-if-the-alias-or-function-name-was-used/
+
+Function Get-TMHowCalled {
+
+    param (
+        [String]$Hello
+    )
+    $Myinvocation
+    write-output "*$($MyInvocation.line)*"
+    If ($MyInvocation.Line -eq 'ghc') {
+        Write-Output -Verbose 'Alias was used.'
+    } ElseIf ($MyInvocation.Line -eq 'Get-TMHowCalled') {
+        Write-Output -Verbose 'Function name was used.'
+    }
+}
+
+New-Alias -Name ghc -Value Get-TMHowCalled 
+
+Get-TMHowCalled -Hello "What"
+
+ghc -Hello There

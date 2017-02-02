45c6c93ee9cd773c1a2f3177595228b8fd292e94
jeffbuenting <jeffbuenting@gmail.com>
2017-02-01 21:54:10 -0500

    Get-Pimage test  added mock for Open-IEWebPage

--- P/P.Tests.ps1
+++ P/P.Tests.ps1
@@ -2,15 +2,26 @@
 $sut = (Split-Path -Leaf $MyInvocation.MyCommand.Path) -replace '\.Tests\.', '.'
 
 import-module $here\p -Force
+import-module C:\Scripts\InternetExporer\InternetExplorer
+
+
+$TestCases = @(
+    @{
+        Url = 'www.huskermax.com'
+    }
+)
+
 
 Describe "Get-pimage" {
 
     # ----- Mock Get-IEWebPage 
-    Mock Get {
-
+    Mock Get-IEWebPage {
+ 
         $WebObject = New-Object -com "InternetExplorer.Application"
-	    $WebObject.visible = $true 
+	    $WebObject.visible = $True
   	    $WebObject.Navigate($Url)
+
+        "New Object"
 
         $IE = New-Object -TypeName -Property @{
             'HTML' = (  Invoke-WebRequest -uri $Url -ErrorAction Stop -Verbose:$false)
@@ -19,11 +30,25 @@ Describe "Get-pimage" {
             'IE' = $WebOject
             'Title' = $Null
         }
+
+write-host "closing"
+       $WebObject.Quit()
+        
+        Write-Output $IE
     }
 
-    It "Returns Url to jpg image" {
+   
+    It "<Url> : Returns Url to jpg image" -TestCases $TestCases {
+        Param (
+           $Url
+        )
+
+        "Url = $Url"
+
+        $Url | Get-IEWebPage
+
         $Images = $IE | Get-PImages
 
-        ($images | Select-Object -first 1 ) -match 'http:\/\/.*\.jpg'
+        $images | Select-Object -first 1 | Should match 'http:\/\/.*\.jpg'
     }
 }
